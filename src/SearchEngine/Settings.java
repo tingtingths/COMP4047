bb1fe8a742b03e0fb05676ce8cb0f27ca936805f
Tony Ting <k999hoshing@gmail.com>
2015-11-06 12:38:15 +0800

    Update workspace directory

--- src/SearchEngine/Settings.java
+++ src/SearchEngine/Settings.java
@@ -6,7 +6,7 @@ import java.io.File;
  * Created by demo on 11/4/2015.
  */
 public class Settings {
-    public static final String workingDir = System.getProperty("user.dir") + File.separator + "spiderWorkspace" + File.separator;
+    public static final String workingDir = "/home/ting/IdeaProjects/COMP4047 Project/SpiderWorkspace" + File.separator;
 
     // refer this to the dict under project directory
     public static final String dictDir = "/home/ting/IdeaProjects/COMP4047 Project/dict/";
